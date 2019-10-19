bfcbd7ff744ac390927bd6f4a9a5707221656a78
Matthew Sloyan <g00348036@gmit.ie>
2019-10-19 21:30:48 +0100

    Fully implemented server and tested it running.

--- src/main/java/ie/gmit/sw/PasswordServer.java
+++ src/main/java/ie/gmit/sw/PasswordServer.java
@@ -2,7 +2,6 @@ package ie.gmit.sw;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -11,6 +10,9 @@ public class PasswordServer {
     private static final Logger logger = Logger.getLogger(PasswordServer.class.getName());
     private static final int PORT = 50551;
 
+    /**
+     * Start gRPC server on port 50551, and build using PasswordServiceImpl
+     */
     private void start() throws IOException {
         grpcServer = ServerBuilder.forPort(PORT)
                 .addService(new PasswordServiceImpl())
@@ -19,9 +21,30 @@ public class PasswordServer {
         logger.info("Server started, listening on " + PORT);
     }
 
+    /**
+     * Stop gRPC server
+     */
     private void stop() {
         if (grpcServer != null) {
             grpcServer.shutdown();
         }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (grpcServer != null) {
+            grpcServer.awaitTermination();
+        }
+    }
+
+    /**
+     * main to run the server
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final PasswordServer passwordServer = new PasswordServer();
+        passwordServer.start();
+        passwordServer.blockUntilShutdown();
     }
 }
