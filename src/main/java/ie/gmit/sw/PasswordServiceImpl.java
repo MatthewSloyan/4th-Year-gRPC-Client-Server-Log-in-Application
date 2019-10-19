1841c6470f1f9b980f09888dd6974757aac673b3
Matthew Sloyan <g00348036@gmit.ie>
2019-10-19 20:43:32 +0100

    Fully implemented hash method in passwordService class with comments. This hashes the password with a salt and is sent back to the client. Testing required.

--- src/main/java/ie/gmit/sw/PasswordServiceImpl.java
+++ src/main/java/ie/gmit/sw/PasswordServiceImpl.java
@@ -1,6 +1,7 @@
 package ie.gmit.sw;
 
 import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 import ie.gmit.ds.HashRequest;
 import ie.gmit.ds.HashResponse;
 import ie.gmit.ds.PasswordServiceGrpc;
@@ -10,9 +11,9 @@ import io.grpc.stub.StreamObserver;
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase {
 
     /**
-     * <pre>
-     * HashRequest is sent as a parameter and waits for sever to respond with hash response.
-     * </pre>
+     *  This method is used to generate a response to send back to the client.
+     *  HashRequest request = The request sent.
+     *  StreamObserver<HashResponse> responseObserver = Is a special interface for the server to call with its response.
      *
      * @param request
      * @param responseObserver
@@ -30,11 +31,26 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
             // Hash the password using the new salt.
             byte[] hashedPassword = Passwords.hash(password, salt);
 
+            // As Google Protocol Buffers method returns a ByteString the byte array must be converted first.
+            // To achieve this I research and found the documentation for ByteStrings
+            // https://developers.google.com/protocol-buffers/docs/reference/java/com/google/protobuf/ByteString
+            ByteString bsSalt = ByteString.copyFrom(salt);
+            ByteString bsHashedPassword = ByteString.copyFrom(hashedPassword);
+
+            // We use the responseObserver’s onNext() method to return the HashRequest,
+            // while populating it with the userId, salt, and hashedPassword.
+            responseObserver.onNext(HashResponse.newBuilder()
+                    .setUserId(request.getUserId())
+                    .setSalt(bsSalt)
+                    .setHashedPassword(bsHashedPassword)
+                    .build());
+
         } catch (RuntimeException ex) {
+            // Send back an error message to client
             responseObserver.onNext(HashResponse.newBuilder().getDefaultInstanceForType());
         }
+        // Specifies that we’ve finished dealing with the RPC.
         responseObserver.onCompleted();
-
     }
 
     /**
