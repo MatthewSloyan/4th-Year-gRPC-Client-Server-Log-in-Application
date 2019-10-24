3509b4e965e955206d13a37266b0b542ed5c2cdc
Matthew Sloyan <g00348036@gmit.ie>
2019-10-24 17:58:21 +0100

    Added method to hash password in testing client.

--- src/main/java/ie/gmit/sw/TestClient.java
+++ src/main/java/ie/gmit/sw/TestClient.java
@@ -1,10 +1,15 @@
 package ie.gmit.sw;
 
+import ie.gmit.ds.HashRequest;
+import ie.gmit.ds.HashResponse;
 import ie.gmit.ds.PasswordServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TestClient {
@@ -26,5 +31,47 @@ public class TestClient {
 
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void hashPassword(HashRequest hashRequest) {
+        logger.info("Hashing password");
+
+        HashResponse result = HashResponse.newBuilder().getDefaultInstanceForType();
+        try {
+            result = syncPasswordService.hash(hashRequest);
+        } catch (StatusRuntimeException ex) {
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+        if (result.getHashedPassword() != null) {
+            logger.info("Successful");
+        } else {
+            logger.warning("Failed to hash password");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TestClient client = new TestClient("localhost", 50551);
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Please enter a userID: ");
+        int userId = console.nextInt();
+
+        System.out.println("Please enter a password: ");
+        String userPassword = console.next();
+
+        // Build a hashRequest object
+        HashRequest hashRequest = HashRequest.newBuilder()
+                .setUserId(userId)
+                .setPassword(userPassword)
+                .build();
+        try {
+            client.hashPassword(hashRequest); // synchronous
+            //client.validatePassword(); // asynchronous
+        } finally {
+            // Don't stop process, keep alive to receive async response
+            Thread.currentThread().join();
+        }
     }
 }
