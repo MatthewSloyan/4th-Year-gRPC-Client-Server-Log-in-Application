561319c207fd0f4271b3f0d677db683c17738516
Matthew Sloyan <g00348036@gmit.ie>
2019-11-15 15:28:17 +0000

    Resructured project into two separate maven projects.

--- distributedsystemsproject/src/main/java/ie/gmit/sw/UserAccountServiceResource.java
+++ distributedsystemsproject/src/main/java/ie/gmit/sw/UserAccountServiceResource.java
@@ -1,9 +1,11 @@
 package ie.gmit.sw;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -16,6 +18,8 @@ public class UserAccountServiceResource {
     public UserAccountServiceResource() {
         //Artist testArtist = new Artist(1, "The GZA", "HipHop", 2);
         //artistsMap.put(testArtist.getArtistId(), testArtist);
+
+        // {"userId":10, "userName":"Matthew_S97", "email":"test@gmail.com", "hashedPassword":"sdfsd", "salt":"dsfsd"}
     }
 
     @GET
@@ -26,5 +30,22 @@ public class UserAccountServiceResource {
         // https://docs.oracle.com/javase/7/docs/api/java/util/Collection.html
 
         return usersMap.values();
+    }
+
+//    @POST
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    public void addUsers(@FormParam("user") User user, @Context HttpServletResponse servletResponse) throws IOException {
+//        usersMap.put(user);
+//    }
+
+    @POST
+    public void addUsers(User user) throws IOException {
+        usersMap.put(user.getUserId(), user);
+    }
+
+    @GET
+    @Path("{userId}")
+    public User getUserById(@PathParam("userId") Integer userId) {
+        return usersMap.get(userId);
     }
 }
